1deee4b78d9c35b057b3475b5d350b9443778aa4
nuri <nuriddin@utec.kr>
2023-07-18 10:55:22 +0900

    Code formatting.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
+
+import 'home_page.dart';
 
 void main() {
   runApp(const MainApp());
@@ -14,60 +14,6 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  HomePage({Key? key}) : super(key: key);
-
-  late double deviceWidth;
-
-  @override
-  Widget build(BuildContext context) {
-    deviceWidth = MediaQuery.of(context).size.width;
-    return Scaffold(
-      backgroundColor: const Color(0xFFc8d6e5),
-      body: homePageUI(),
-    );
-  }
-
-  Widget homePageUI() {
-    return Center(
-      child: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const Padding(
-              padding: EdgeInsets.only(top: 20),
-              child: Text(
-                'BLE TEST',
-                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-              ),
-            ),
-            SizedBox(
-              height: 130,
-              width: 130,
-              child: Image.asset(
-                "assets/image/bluetooth-icon-6 copy.png",
-              ),
-            ),
-            SizedBox(
-              width: deviceWidth,
-              height: 45,
-              child: ElevatedButton(
-                onPressed: () {},
-                child: const Text(
-                  "Receive",
-                  style: TextStyle(fontSize: 18, color: Colors.white),
-                ),
-              ),
-            )
-          ],
-        ),
-      ),
     );
   }
 }
